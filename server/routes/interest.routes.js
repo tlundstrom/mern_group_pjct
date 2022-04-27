8f694ae391e7b038484e25503b2207b58b896386
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-04-27 13:54:11 -0700

    Added Interests Mapping

--- server/routes/interest.routes.js
+++ server/routes/interest.routes.js
@@ -3,7 +3,7 @@ const InterestController = require("../controllers/interest.controller");
 
 module.exports = (app) => {
 	app.post("/api/events/:eventId/interests/create", authenticate, InterestController.createInterest);
-	app.get("/api/events/:eventId/interests", InterestController.getAllInterests);
+	app.get("/api/interests", InterestController.getAllInterests);
 	app.put("/api/events/:eventId/interests/:id", authenticate, InterestController.updateOneInterest);
 	app.delete("/api/events/:eventId/interests/:id", authenticate, InterestController.deleteOneInterest);
 };
